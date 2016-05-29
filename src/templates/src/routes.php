d7387d46391eeffb0b3ec6e9cba159e151f8b872
lembarek <lembarek2011@gmail.com>
2016-05-29 10:51:26 +0100

    change {{name}} to {{name}}:: in src/templates/src/routes.php

--- src/templates/src/routes.php
+++ src/templates/src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['as' => '{{name}}', 'middleware' => ['web'], 'namespace' => '{{Vendor}}\{{Name}}\Controllers'], function () {
+Route::group(['as' => '{{name}}::', 'middleware' => ['web'], 'namespace' => '{{Vendor}}\{{Name}}\Controllers'], function () {
 
 
 });
