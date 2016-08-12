ceea8656f9173a94d56c5fc75cbb7028606e7fe1
lembarek <lembarek2011@gmail.com>
2016-08-12 11:26:31 +0100

    small improvement

--- src/templates/src/routes.php
+++ src/templates/src/routes.php
@@ -1,6 +1,10 @@
 <?php
 
-Route::group(['as' => '{{name}}::', 'middleware' => ['web'], 'namespace' => '{{Vendor}}\{{Name}}\Controllers'], function () {
+Route::group([
+    'as' => '{{name}}::',
+    'middleware' => ['web'],
+    'namespace' => '{{Vendor}}\{{Name}}\Controllers'
+], function () {
 
 
 });
